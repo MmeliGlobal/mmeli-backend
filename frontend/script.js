9cbf6c93e983b3cf1d492bc6d18c1e52416a304c
MmeliGlobal <isaiahmmeli@gmail.com>
2026-06-05 22:44:56 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,6 @@
 // ========== SUPABASE CLIENT ==========
 const SUPABASE_URL = 'https://proljdccjrifqgbmsyco.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByb2xqZGNjanJpZnFnYm1zeWNvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTc4ODAxOSwiZXhwIjoyMDkxMzY0MDE5fQ.VltzBUq-bLvu0Ny4jPy1kBp5E-4hffQgqFpqHrRWlZA';
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ========== GLOBALS ==========
 let allProducts = [];
